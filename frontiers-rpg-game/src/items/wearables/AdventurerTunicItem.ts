c557020d4b6722839e6d7d9401d10d8bf960c077
ArkDev <me@braydonb.com>
2025-06-23 15:11:03 -0700

    Added basic initial wearable item for testing.

---
+++ frontiers-rpg-game/src/items/wearables/AdventurerTunicItem.ts
@@ -0,0 +1,13 @@
+import BaseWearableItem, { WearableSlot } from '../BaseWearableItem';
+
+export default class AdventurerTunicItem extends BaseWearableItem {
+  static readonly id = 'adventurer_tunic';
+  static readonly name = 'Adventurer Tunic';
+  static readonly iconImageUri = 'icons/items/adventurer-tunic.png';
+  static readonly consumeCooldownMs = 400;
+  static readonly description = `A basic tunic, nothing special about it.`;
+  static readonly buyPrice = 20;
+  static readonly sellPrice = 5;
+
+  static readonly slot: WearableSlot = 'armor';
+}
