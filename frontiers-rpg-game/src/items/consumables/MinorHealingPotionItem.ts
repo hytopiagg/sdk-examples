bd462c6a88c3adbb7e662f0b3732388e61c9bede
ArkDev <me@braydonb.com>
2025-06-30 22:57:32 -0700

    Made minor healing potion cheaper.

--- frontiers-rpg-game/src/items/consumables/MinorHealingPotionItem.ts
+++ frontiers-rpg-game/src/items/consumables/MinorHealingPotionItem.ts
@@ -7,7 +7,7 @@ export default class MinorHealingPotionItem extends BaseConsumableItem {
   static readonly iconImageUri = 'icons/items/minor-healing-potion.png';
   static readonly consumeCooldownMs = 2000;
   static readonly description = `A common remedy used by Frontier travelers and novice adventurers.`;
-  static readonly buyPrice = 50;
+  static readonly buyPrice = 25;
   static readonly sellPrice = 5;
   static readonly stackable = true;
   static readonly consumeRequiresDamaged = true;
